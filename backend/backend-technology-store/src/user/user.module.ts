168e423cd9625fe251d0c1863307a8c0270be655
Andy Torres <andy18wrld@gmail.com>
2025-10-16 22:10:52 -0400

    fix:importado

--- backend/backend-technology-store/src/user/user.module.ts
+++ backend/backend-technology-store/src/user/user.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from 'src/guard/roles.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
